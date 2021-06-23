123a0ceb58097f74fb6236b25ad05afae6f9fe83
Irving Zamora <irvingzamora28@gmail.com>
2021-06-23 17:19:35 -0600

    Modified context

--- src/context.js
+++ src/context.js
@@ -5,8 +5,25 @@ const RoomContext = React.createContext()
 
 class RoomProvider extends Component {
     state ={
-        greeting: 'hello',
-        name: 'John'
+        rooms:[],
+        sortedRooms: [],
+        featuredRooms: [],
+        loading: true
+    }
+
+    componentDidMount() {
+        let rooms = this.formatData(items)
+    }
+
+    formatData(items) {
+        let tempItems = items.map((item) => {
+            let id = items.sys.id
+            let images = item.fields.images.map(image => image.fields.file.url)
+            let room = {...item.fields, images, id}
+            return room
+        })
+
+        return tempItems
     }
     render() {
         return (
